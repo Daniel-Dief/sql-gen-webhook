c4114ab15aa48b8109f719de7497e0be3b2fcc7d
Daniel-Dief <danieldiefsantos@gmail.com>
2025-08-12 16:39:22 -0300

    feat: chamar a ia de forma assíncrona

--- app/ia.py
+++ app/ia.py
@@ -2,9 +2,12 @@ from langchain_ollama import OllamaLLM
 from langchain_core.prompts import PromptTemplate
 from pathlib import Path
 from app.types.modelsTypes import ModelType
-from random import random
+from app.helpers.save_response import save_response
+from app.helpers.uuid import generate_uuid
+from threading import Thread
+from app.types.responseAPI import ResponseAPI
 
-def gen_query(prompt: str, model: ModelType):
+def gen_query(prompt: str, model: ModelType) -> ResponseAPI:
     template = Path(f"./app/prompts/{model.prompt_file}").read_text(encoding="utf-8")
     schema = Path(f"./app/models/{model.file_name}").read_text(encoding="utf-8")
 
@@ -12,14 +15,26 @@ def gen_query(prompt: str, model: ModelType):
         input_variables=["schema", "prompt"],
         template=template
     )
-
     final_prompt = prompt_template.format(schema=schema, prompt=prompt)
 
+    uuid = generate_uuid()
+
+    Thread(
+        target=request_ai,
+        args=(final_prompt, uuid)
+    ).start()
+
+    return ResponseAPI(
+        id=uuid,
+        model=model.name,
+        prompt=prompt
+    )
+
+def request_ai(prompt: str, uuid: str) -> None:
     llm_model = OllamaLLM(model="gpt-oss:20b")
+    response = llm_model.invoke(prompt)
 
-    response = llm_model.invoke(final_prompt)
-
-    uuid = str(random()).split(".")[1]
-
-    with open(f"./app/responses/{uuid}.sql", "w", encoding="utf-8") as f:
-        f.write(response)
+    save_response(
+        uuid,
+        response
+    )
